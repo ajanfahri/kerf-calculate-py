65385e48ed00ea91744a1f15eb4d07392f9e6734
Fahreddin <fahreddintokatli@hotmail.com>
2024-05-25 12:42:55 +0300

    import pandas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import kerf_hesaplama
 import veritabani
+import pandas as pd
 
 st.title("Kerf ve Offset Hesaplama")
 
@@ -35,19 +36,9 @@ else:
 if st.button("Hesapla"):
     results = kerf_hesaplama.kerf_width_bul(material, current, gases, float(thickness))
 
-    # Sonuçları tablo şeklinde gösterme
+     # Sonuçları DataFrame'e dönüştürme
+    df = pd.DataFrame(results, columns=["Açı", "Top Offset", "Bottom Offset"])
+
+    # Sonuçları tablo şeklinde gösterme (st.dataframe ile)
     st.subheader("Sonuçlar:")
-    if results:
-        st.write(
-            f"""
-            | Açı | Top Offset | Bottom Offset |
-            |---|---|---|
-            """
-        )
-        for aci, top_offset, bottom_offset in results:
-            if top_offset is not None and bottom_offset is not None:
-                st.write(f"| {aci} | {top_offset:.3f} | {bottom_offset:.3f} |")
-            else:
-                st.write(f"| {aci} | Veri Yok | Veri Yok |")
-    else:
-        st.write("Belirtilen değerlere uygun kayıt bulunamadı.")
+    st.dataframe(df.style.format("{:.3f}"))  # Ondalık sayıları 3 basamağa formatla
